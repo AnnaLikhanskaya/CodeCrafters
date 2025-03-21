6b3d5de9de436ba15732868e6b0bf9039c80b821
sergey kuznetsov <mazdai.88@mail.ru>
2025-03-21 19:40:51 +0300

    New interface CarComparator

--- src/CarComparator.java
+++ src/CarComparator.java
@@ -1,23 +1,41 @@
 import java.util.Comparator;
 
+/*сравнение происходит по всем полям. начинаем с марки авто*/
 public interface CarComparator extends Comparator<Car> {
     @Override
     default int compare(Car car1, Car car2) {
 
         int compareModel = car1.getModel().compareTo(car2.getModel());
         if (compareModel != 0) {
-            return compareModel;
+            return compareModel; /*если модель разная - возвращаем результат, если нет - идем дальше*/
         }
 
         int comparePower = car1.getPower() - car2.getPower();
         if (comparePower != 0) {
-            return comparePower;
+            return comparePower; /*если мощность разная - возвращаем результат, если нет - идем дальше*/
         }
 
         int compareYear = car1.getYear() - car2.getYear();
         if (compareYear != 0) {
-            return compareYear;
+            return compareYear; /*сравниваем год*/
         }
         return 0;
     }
 }
+
+/*пример использования*/
+/*    public static void main(String[] args) throws Exception {
+
+        CarComparator comp = new CarComparator() {
+            @Override
+            public int compare(Car car1, Car car2) {
+                return CarComparator.super.compare(car1, car2);
+                }
+            };
+
+        List<Car> cars = List.of();
+
+        cars.stream()
+                 .sorted(comp)
+                 .forEach(System.out::println);
+    }*/
