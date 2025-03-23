ebf0f29f9d8de10a7db102a64b7708ccf890bfe6
AnnaLikhanskaya <a.lihansky@yandex.ru>
2025-03-23 13:32:11 +0300

    fix: menu

--- src/main/java/ru/home/Main.java
+++ src/main/java/ru/home/Main.java
@@ -1,300 +1,121 @@
 package java.ru.home;
 
-import ru.home.model.Book;
-import ru.home.model.Car;
-import ru.home.model.RootVegetable;
-import ru.home.model.comparator.BookTitleComparator;
-import ru.home.model.comparator.CarModelComparator;
-import ru.home.model.comparator.RootVegetableTypeComparator;
-import ru.home.service.FileService;
-import ru.home.service.ManualService;
-import ru.home.service.RandomService;
-import ru.home.service.SortService;
-import ru.home.service.impl.FileServiceImpl;
-import ru.home.service.impl.ManualServiceImpl;
-import ru.home.service.impl.MergeSortServiceImpl;
-import ru.home.service.impl.RandomServiceImpl;
-import ru.home.storage.Storage;
-import ru.home.util.CustomArrayList;
-
+import java.awt.print.Book;
+import java.ru.home.model.Car;
+import java.ru.home.model.RootCrop;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        FileService fileService = new FileServiceImpl();
-//        RandomService randomService = new RandomServiceImpl();
-//        ManualService manualService = new ManualServiceImpl();
-//        SortService<Car> carSortService = new MergeSortServiceImpl<>();
-//        SortService<Book> bookSortService = new MergeSortServiceImpl<>();
-//        SortService<RootVegetable> rootVegetableSortService = new MergeSortServiceImpl<>();
-//        SearchService<Car> carSearchService = new BinarySearchService<>();
-//        SearchService<Book> bookSearchService = new BinarySearchService<>();
-//        SearchService<RootVegetable> rootVegetableSearchService = new BinarySearchService<>();
-
-        Storage<Car> carStorage = new CustomArrayList<>();
-        Storage<Book> bookStorage = new CustomArrayList<>();
-        Storage<RootVegetable> rootVegetableStorage = new CustomArrayList<>();
-
-        System.out.println("Добро пожаловать в приложение сортировки!");
         while (true) {
-            System.out.println("Выберите тип объектов:");
-            System.out.println("1. Автомобили");
-            System.out.println("2. Книги");
-            System.out.println("3. Корнеплоды");
-            System.out.println("4. Выход");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
+            System.out.println("Выберите тип данных (1 - Автомобиль, 2 - Книга, 3 - Корнеплод):");
+            int type = scanner.nextInt();
+            System.out.println("Выберите способ ввода данных (1 - Вручную, 2 - Из файла, 3 - Случайно):");
+            int inputMethod = scanner.nextInt();
+            DataInputStrategy<Car> car = null;
+            DataInputStrategy<Book> book = null;
+            DataInputStrategy<RootCrop> rootVegetable = null;
+            CustomArrayList<Car> listCar = null;
+            CustomArrayList<RootCrop> listRoot = null;
+            CustomArrayList<Book> listBook = null;
+            switch (inputMethod) {
                 case 1:
-                    handleCarMenu(scanner, fileService, randomService, manualService, carSortService,
-                            carStorage, carSearchService);
+                    if (type == 1) {
+                        car = new ManualInputCar(scanner);
+                    }
+                    if (type == 2) {
+                        book = new ManualInputBook(scanner);
+                    }
+                    if (type == 3) {
+                        rootVegetable = new ManualInputRootVegetable(scanner);
+                    }
                     break;
                 case 2:
-                    handleBookMenu(scanner, fileService, randomService, manualService, bookSortService,
-                            bookStorage, bookSearchService);
+                    if (type == 1) {
+                        car = new FileInputCar();
+                    }
+                    if (type == 2) {
+                        book = new FileInputBook();
+                    }
+                    if (type == 3) {
+                        rootVegetable = new FileInputRootVegetable();
+                    }
                     break;
                 case 3:
-                    handleRootVegetableMenu(scanner, fileService, randomService, manualService,
-                            rootVegetableSortService, rootVegetableStorage, rootVegetableSearchService);
+                    if (type == 1) {
+                        car = new RandomInputCar();
+                    }
+                    if (type == 2) {
+                        book = new RandomInputBook();
+                    }
+                    if (type == 3) {
+                        rootVegetable = new RandomInputRootVegetable();
+                    }
                     break;
-                case 4:
-                    System.out.println("Выход из программы. До свидания!");
-                    return;
+
                 default:
-                    System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
+                    System.out.println("Неверный выбор");
+                    continue;
+            }
+
+            if (car != null) {
+                listCar = car.inputData();
+                System.out.println("До сортировки: " + listCar);
+                MergeSort<Car> sorter = new MergeSort<>();
+                sorter.sort(listCar);
+                System.out.println("После сортировки: " + listCar);
+            } else if (book != null) {
+                listBook = book.inputData();
+                System.out.println("До сортировки: " + listBook);
+                MergeSort<Book> sorter = new MergeSort<>();
+                sorter.sort(listBook);
+                System.out.println("После сортировки: " + listBook);
+            } else if (rootVegetable != null) {
+                listRoot = rootVegetable.inputData();
+                System.out.println("До сортировки: " + listRoot);
+                MergeSort<RootVegetable> sorter = new MergeSort<>();
+                sorter.sort(listRoot);
+                System.out.println("После сортировки: " + listRoot);
+            }
+
+            System.out.println("Хотите выполнить бинарный поиск? (1 - Да, 2 - Нет):");
+            int searchChoice = scanner.nextInt();
+            if (searchChoice == 1) {
+                System.out.println("Введите элемент для поиска:");
+                if (listCar != null) {
+                    BinarySearch<Car> binarySearch = new BinarySearch<>();
+                    Car carFind = new Car.Builder().power(200).model("Mazda").year(2019).build();
+                    int index = binarySearch.search(listCar, carFind);
+                    check(index);
+                }
+                if (listBook != null) {
+                    BinarySearch<Book> binarySearch = new BinarySearch<>();
+                    Book bookFind = new Book.Builder().author("Lev").title("War and peace").pages(500).build();
+                    int index = binarySearch.search(listBook, bookFind);
+                    check(index);
+                }
+                if (listRoot != null) {
+                    BinarySearch<RootCrop> binarySearch = new BinarySearch<>();
+                    RootCrop rootFind = new RootCrop.Builder().type("Carrot").weight(2).color("orange").build();
+                    int index = binarySearch.search(listRoot, rootFind);
+                    check(index);
+                }
+            }
+
+            System.out.println("Хотите выйти? (1 - Да, 2 - Нет):");
+            int exitChoice = scanner.nextInt();
+            if (exitChoice == 1) {
+                break;
             }
         }
     }
 
-    private static void handleCarMenu(Scanner scanner, FileService fileService, RandomService randomService,
-                                      ManualService manualService, SortService<Car> sortService, Storage<Car> carStorage,
-                                      SearchService<Car> carSearchService) {
-        while (true) {
-            System.out.println("Выберите действие для автомобилей:");
-            System.out.println("1. Заполнить массив из файла");
-            System.out.println("2. Заполнить массив случайными данными");
-            System.out.println("3. Заполнить массив вручную");
-            System.out.println("4. Бинарный поиск");
-            System.out.println("5. Вернуться в главное меню");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Идет заполнение массива автомобилей из файла...");
-                    Car[] cars = fileService.fillFromFileForCars("cars.txt");
-                    for (Car car : cars) {
-                        carStorage.add(car);
-                        System.out.println("Добавлен автомобиль: " + car.getModel());
-                    }
-                    System.out.println("Данные из файла успешно загружены.");
-                    break;
-                case 2:
-                    System.out.println("Идет заполнение массива автомобилей случайными данными...");
-                    System.out.print("Введите количество автомобилей: ");
-                    int size = scanner.nextInt();
-                    scanner.nextLine();
-                    cars = randomService.fillRandomCars(size);
-                    for (Car car : cars) {
-                        carStorage.add(car);
-                        System.out.println("Добавлен автомобиль: " + car.getModel());
-                    }
-                    break;
-                case 3:
-                    System.out.println("Идет заполнение массива автомобилей вручную...");
-                    System.out.print("Введите количество автомобилей: ");
-                    size = scanner.nextInt();
-                    scanner.nextLine();
-                    cars = manualService.fillManuallyCars(scanner, size);
-                    for (Car car : cars) {
-                        carStorage.add(car);
-                    }
-                    break;
-                case 4:
-                    if (carStorage.getAll().size() > 0) {
-                        System.out.println("Идет выполнение бинарного поиска в массиве автомобилей...");
-                        System.out.print("Введите модель автомобиля для поиска: ");
-                        String model = scanner.nextLine();
-                        Car[] carsArray = carStorage.getAll().toArray(new Car[0]);
-
-
-                        sortService.sort(carsArray, new CarModelComparator());
-
-
-                        Car searchKey = new Car.Builder().setModel(model).setPower(100).setYear(2000).build();
-
-                        int index = carSearchService.search(carsArray, searchKey, new CarModelComparator());
-                        if (index != -1) {
-                            System.out.println("Автомобиль найден на позиции: " + index);
-                        } else {
-                            System.out.println("Автомобиль не найден.");
-                        }
-                    } else {
-                        System.out.println("Массив автомобилей пуст. Пожалуйста, заполните его перед поиском.");
-                    }
-                    break;
-                case 5:
-                    return;
-                default:
-                    System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
-            }
-        }
-    }
-
-    private static void handleBookMenu(Scanner scanner, FileService fileService, RandomService randomService,
-                                       ManualService manualService, SortService<Book> sortService,
-                                       Storage<Book> bookStorage, SearchService<Book> bookSearchService) {
-        while (true) {
-            System.out.println("Выберите действие для книг:");
-            System.out.println("1. Заполнить массив из файла");
-            System.out.println("2. Заполнить массив случайными данными");
-            System.out.println("3. Заполнить массив вручную");
-            System.out.println("4. Бинарный поиск");
-            System.out.println("5. Вернуться в главное меню");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Идет заполнение массива книг из файла...");
-                    Book[] books = fileService.fillFromFileForBooks("books.txt");
-                    for (Book book : books) {
-                        bookStorage.add(book);
-                        System.out.println("Добавлена книга: " + book.getTitle());
-                    }
-                    System.out.println("Данные из файла успешно загружены.");
-                    break;
-                case 2:
-                    System.out.println("Идет заполнение массива книг случайными данными...");
-                    System.out.print("Введите количество книг: ");
-                    int size = scanner.nextInt();
-                    scanner.nextLine();
-                    books = randomService.fillRandomBooks(size);
-                    for (Book book : books) {
-                        bookStorage.add(book);
-                        System.out.println("Добавлена книга: " + book.getTitle());
-                    }
-                    break;
-                case 3:
-                    System.out.println("Идет заполнение массива книг вручную...");
-                    System.out.print("Введите количество книг: ");
-                    size = scanner.nextInt();
-                    scanner.nextLine();
-                    books = manualService.fillManuallyBooks(scanner, size);
-                    for (Book book : books) {
-                        bookStorage.add(book);
-                    }
-                    break;
-                case 4:
-                    if (bookStorage.getAll().size() > 0) {
-                        System.out.println("Идет выполнение бинарного поиска в массиве книг...");
-                        System.out.print("Введите название книги для поиска: ");
-                        String title = scanner.nextLine();
-                        Book[] booksArray = bookStorage.getAll().toArray(new Book[0]);
-
-                        sortService.sort(booksArray, new BookTitleComparator());
-
-                        Book searchKey = new Book.Builder().setTitle(title).setAuthor("Unknown").setPages(1).build();
-
-                        int index = bookSearchService.search(booksArray, searchKey, new BookTitleComparator());
-                        if (index != -1) {
-                            System.out.println("Книга найдена на позиции: " + index);
-                        } else {
-                            System.out.println("Книга не найдена.");
-                        }
-                    } else {
-                        System.out.println("Массив книг пуст. Пожалуйста, заполните его перед поиском.");
-                    }
-                    break;
-                case 5:
-                    return;
-                default:
-                    System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
-            }
-        }
-    }
-
-    private static void handleRootVegetableMenu(Scanner scanner, FileService fileService, RandomService randomService,
-                                                ManualService manualService, SortService<RootVegetable> sortService,
-                                                Storage<RootVegetable> rootVegetableStorage,
-                                                SearchService<RootVegetable> rootVegetableSearchService) {
-        while (true) {
-            System.out.println("Выберите действие для корнеплодов:");
-            System.out.println("1. Заполнить массив из файла");
-            System.out.println("2. Заполнить массив случайными данными");
-            System.out.println("3. Заполнить массив вручную");
-            System.out.println("4. Бинарный поиск");
-            System.out.println("5. Вернуться в главное меню");
-
-            int choice = scanner.nextInt();
-            scanner.nextLine();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Идет заполнение массива корнеплодов из файла...");
-                    RootVegetable[] rootVegetables = fileService.fillFromFileForRootVegetables("root_vegetables.txt");
-                    for (RootVegetable rootVegetable : rootVegetables) {
-                        rootVegetableStorage.add(rootVegetable);
-                        System.out.println("Добавлен корнеплод: " + rootVegetable.getType());
-                    }
-                    System.out.println("Данные из файла успешно загружены.");
-                    break;
-                case 2:
-                    System.out.println("Идет заполнение массива корнеплодов случайными данными...");
-                    System.out.print("Введите количество корнеплодов: ");
-                    int size = scanner.nextInt();
-                    scanner.nextLine();
-                    rootVegetables = randomService.fillRandomRootVegetables(size);
-                    for (RootVegetable rootVegetable : rootVegetables) {
-                        rootVegetableStorage.add(rootVegetable);
-                        System.out.println("Добавлен корнеплод: " + rootVegetable.getType());
-                    }
-                    break;
-                case 3:
-                    System.out.println("Идет заполнение массива корнеплодов вручную...");
-                    System.out.print("Введите количество корнеплодов: ");
-                    size = scanner.nextInt();
-                    scanner.nextLine();
-                    rootVegetables = manualService.fillManuallyRootVegetables(scanner, size);
-                    for (RootVegetable rootVegetable : rootVegetables) {
-                        rootVegetableStorage.add(rootVegetable);
-                    }
-                    break;
-                case 4:
-                    if (rootVegetableStorage.getAll().size() > 0) {
-                        System.out.println("Идет выполнение бинарного поиска в массиве корнеплодов...");
-                        System.out.print("Введите тип корнеплода для поиска: ");
-                        String type = scanner.nextLine();
-                        RootVegetable[] rootVegetablesArray = rootVegetableStorage.getAll()
-                                .toArray(new RootVegetable[0]);
-
-                        sortService.sort(rootVegetablesArray, new RootVegetableTypeComparator());
-
-                        RootVegetable searchKey = new RootVegetable.Builder().setType(type).setWeight(0.1)
-                                .setColor("Unknown").build();
-
-                        int index = rootVegetableSearchService.search(rootVegetablesArray, searchKey,
-                                new RootVegetableTypeComparator());
-                        if (index != -1) {
-                            System.out.println("Корнеплод найден на позиции: " + index);
-                        } else {
-                            System.out.println("Корнеплод не найден.");
-                        }
-                    } else {
-                        System.out.println("Массив корнеплодов пуст. Пожалуйста, заполните его перед поиском.");
-                    }
-                    break;
-                case 5:
-                    return;
-                default:
-                    System.out.println("Неверный выбор. Пожалуйста, попробуйте снова.");
-            }
+    private static void check(int index) {
+        if (index != -1) {
+            System.out.println("Элемент найден на позиции: " + index);
+        } else {
+            System.out.println("Элемент не найден");
         }
     }
 }
